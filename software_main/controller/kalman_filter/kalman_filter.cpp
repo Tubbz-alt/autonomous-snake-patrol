0e2f42620e099001217d8b7aa89ce2eaff77a54e
Matt Blomquist <blomquist.matt@gmail.com>
2017-04-06 08:07:23 -0700

    Create kalman_filter.cpp
    feat: add kalman filter template.

---
+++ software_main/controller/kalman_filter/kalman_filter.cpp
@@ -0,0 +1,25 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init() {
+
+  // Initialize Filter Variables
+}
+
+void KalmanFilter::Predict() {
+
+  // State prediction algorithm
+  
+}
+
+void KalmanFilter::Update() {
+
+  // State update step
+  
+}
