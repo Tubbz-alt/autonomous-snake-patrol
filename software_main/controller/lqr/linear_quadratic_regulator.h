c1a9145eb8759f1253fcd29d8941385b94b24463
Matt Blomquist <blomquist.matt@gmail.com>
2017-04-06 07:57:52 -0700

    Create linear_quadratic_regulator.h
    feat: add template header file for linear quadratic regulator.

---
+++ software_main/controller/lqr/linear_quadratic_regulator.h
@@ -0,0 +1,15 @@
+#ifndef linear_quadratic_regulator
+#define linear_quadratic_regulator
+
+#include
+
+class linear_quadratic_regulator {
+
+public:
+
+  // Add class variables
+  
+  
+};
+
+#endif /* linear_quadratic_regulator */
