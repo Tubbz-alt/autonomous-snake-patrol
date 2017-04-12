d749eb9262c8252e3929a6c1f72eb89cff476a2f
Matt Blomquist <blomquist.matt@gmail.com>
2017-04-12 07:42:14 -0700

    Create monitor.h
    feat: add temp header file for monitor routine.

---
+++ software_aux/system_monitor/monitor.h
@@ -0,0 +1,17 @@
+#ifndef monitor
+#define monitor
+
+// Add include statements
+
+class monitor {
+
+public:
+
+  // Initialization here
+  
+  // Constructor
+  monitor();
+  
+};
+
+#endif /* monitor */
